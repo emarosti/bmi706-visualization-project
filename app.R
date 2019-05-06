ed11dd19c3d2a9fbd0b9b3a9756d13fc45291828
Eliana Marostica <emarosti@wellesley.edu>
2019-05-06 17:24:24 -0400

    crosstalk functionality in first visualization, corresponding data tidying updates. Highlighting functionality still buggy in choropleth map of first visualization

--- app.R
+++ app.R
@@ -12,6 +12,7 @@ library(plotly)
 library(crosstalk)
 library(shinythemes)
 library(viridis)
+library(ggthemes)
 
 load("data/deathrate.Rdata")
 
@@ -21,24 +22,26 @@ ui <- fluidPage(
     theme = shinytheme("sandstone"),
     
     #Title
-    titlePanel(h1("Motor Vehicle-Related Deaths in the United States")),
+    titlePanel("Motor Vehicle-Related Deaths in the United States"),
     h3("Eliana Marostica, Maria Nakhoul, and Sunny Mahesh"),
     HTML('<p> Final Project for <a href="https://dbmi.hms.harvard.edu/education/courses/bmi-706">BMI 706</a> in
          <a href="https://dbmi.hms.harvard.edu/">The Blavatnik Institute Department of Biomedical Informatics</a></p>'),
     
     navbarPage("",
                
-               tabPanel("Vis1",
+               tabPanel("By Age and Gender",
                         value = "B", 
-                        h4("Summary Statistics - Counts"),
+                        h3("Death Rates by Age and Gender in the United States, 2012 & 2014"),
                         sidebarPanel(
                             #radio buttons for age group
                             radioButtons("mapyear", "Year", choices=c("2012","2014")),
-                            selectInput("mapstrat", "By Age or Gender", choices=unique(ocdr$Var))
+                            selectizeInput("mapstrat", "By Age or Gender", choices=unique(ocdr$Var))
                         ),
                         mainPanel(
-                            plotlyOutput("newdatamap")
-                            )
+                            plotlyOutput("boxplot"),
+                            verbatimTextOutput("boxclick")
+                            ),
+                        fluidRow(column(plotlyOutput("olddatamap"), width=12))
                         ),
                
                tabPanel("Vis2", value = "A", 
@@ -57,9 +60,50 @@ ui <- fluidPage(
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+    
+    output$boxplot <- renderPlotly({
+        p <- ocdr_tidy %>%
+            unite(col="Label", Age, Gender, Year, sep=", ",remove=F) %>%
+            #group_by(Year) %>%
+            ggplot(aes(x=Label,y=Death_Rate)) +
+            geom_boxplot(aes(fill=Year)) +
+            theme_few() +
+            theme(axis.title.x = element_text(),
+                  axis.title.y = element_text(),
+                  axis.text.x = element_text(angle=-45,size=8,hjust=1)) +
+            scale_fill_viridis_d()
+        ggplotly(p)
+        
+    })
+    
+    output$boxclick <- renderPrint({
+        labels <- ocdr_tidy %>%
+            unite(col="Label", Age, Gender, Year, sep=", ",remove=F) %>%
+            select(Label) %>%
+            distinct() %>%
+            arrange(Label)
+        
+        d <- event_data("plotly_click")
+        if (is.null(d)) {
+            return(paste("Use the dropdown menu to stratify by age or gender, or select directly on the boxplot"))
+        }
+        else{
+            #return(input$mapyear)
+            updateRadioButtons(session,"mapyear",label="Year", choices=c("2012","2014"), selected=ifelse(sum(d$curveNumber) == 0,"2012","2014"))
+            updateSelectizeInput(session,"mapstrat", selected= ifelse(unique(d$x) == 1 | unique(d$x) == 2, "Age 0-20",
+                                                                   ifelse(unique(d$x) == 3 | unique(d$x) == 4, "Age 21-34",
+                                                                          ifelse(unique(d$x) == 5 | unique(d$x) == 6, "Age 35-54",
+                                                                                 ifelse(unique(d$x) == 7 | unique(d$x) == 8, "Age 55+",
+                                                                                        ifelse(unique(d$x) == 9 | unique(d$x) == 10, "All Ages",
+                                                                                               ifelse(unique(d$x) == 11 | unique(d$x) == 12,"Female", "Male"))))))) 
+            return(paste("Use the dropdown menu to stratify by age or gender, or select directly on the boxplot"))
+        }
+        
+    })
+    
 
-    output$newdatamap <- renderPlotly({
+    output$olddatamap <- renderPlotly({
         
         # give state boundaries a white border
         l <- list(color = toRGB("white"), width = 2)
@@ -67,22 +111,43 @@ server <- function(input, output) {
         g <- list(
             scope = 'usa',
             projection = list(type = 'albers usa'),
-            showlakes = TRUE,
-            lakecolor = toRGB('white')
+            showlakes = FALSE
         )
-
-        ocdr %>%
-            filter(Year == "2012", Var == "All Ages", !is.na(code)) %>%
-            plot_geo(., locationmode = 'USA-states') %>%
+        
+        sd <- SharedData$new(ocdr, key=~State)
+    
+        
+        p1 <- sd %>%
+            plot_geo(.,locationmode = 'USA-states') %>%
+            hide_legend() %>%
+            filter(Year == input$mapyear, Var == input$mapstrat, !is.na(code)) %>%
             add_trace(
                 z = ~Death_Rate, color=~Death_Rate, text = ~Location, locations = ~code,
                 colors = viridis(n=5,direction=-1)
             ) %>%
-            colorbar(title = "Death Rate per 100,000") %>%
+            hide_colorbar() %>%
             layout(
                 title = paste('Motor Vehicle Related Deaths', "All Ages",sep=", "),
                 geo = g
-            )
+            ) %>%
+            highlight(selected = attrs_selected(opacity = 0.5))
+        
+        p2 <- sd %>%
+            plot_ly() %>%
+            filter(Year == input$mapyear, Var == input$mapstrat) %>%
+            group_by(State) %>%
+            arrange(Death_Rate) %>%
+            hide_legend() %>%
+            add_bars(x=~State, y=~Death_Rate, marker = list(color = "grey"), name = "Death Rates") %>%
+            layout(xaxis= list(categoryorder = "array",
+                               categoryarray = ~State),
+                   barmode='overlay',
+                   font=list(size=8))
+        
+        sub2 <- subplot(p1, p2, nrows=1) %>%
+            layout(title="Motor Vehicle Related Deaths, Stratified")
+        
+        sub2
     })
     
 }
--- src/tidy_data.R
+++ src/tidy_data.R
@@ -2,18 +2,44 @@
 # Eliana Marostica
 # May 4, 2019
 
+library(tidyverse)
+
 iddr <- read_csv("data/Impaired_Driving_Death_Rate__by_Age_and_Gender__2012___2014__All_States.csv")
-ocdr <- read_csv("data/Motor_Vehicle_Occupant_Death_Rate__by_Age_and_Gender__2012___2014__All_States.csv")
+ocdr_orig <- read_csv("data/Motor_Vehicle_Occupant_Death_Rate__by_Age_and_Gender__2012___2014__All_States.csv")
 sebt <- read_csv("data/Percentage_of_Drivers_and_Front_Seat_Passengers_Wearing_Seat_Belts__2012___2014__All_States.csv")
 codes <- read.csv("https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv") %>%
   select(code, state)
 
-ocdr <- ocdr %>%
-  mutate(code = codes$code[match(ocdr$State,codes$state)]) %>%
+ocdr <- ocdr_orig %>%
+  mutate(code = codes$code[match(ocdr_orig$State,codes$state)]) %>%
   gather(`All Ages, 2012`, `All Ages, 2014`, `Age 0-20, 2012`,`Age 0-20, 2014`,`Age 21-34, 2012`,`Age 21-34, 2014`,
          `Age 35-54, 2012`,`Age 35-54, 2014`,`Age 55+, 2012`,`Age 55+, 2014`, `Male, 2012`,`Male, 2014`,
          `Female, 2012`,`Female, 2014`, key="Var and Year", value="Death_Rate") %>%
   separate(`Var and Year`, into = c("Var", "Year"), sep=", ") %>%
   filter(!is.na(State))
 
-save(ocdr,iddr, sebt, file="data/deathrate.Rdata")
+ocdr_gender <- ocdr_orig %>%
+  mutate(code = codes$code[match(ocdr_orig$State,codes$state)]) %>%
+  filter(!is.na(State)) %>%
+  gather(`Male, 2012`,`Male, 2014`, `Female, 2012`,`Female, 2014`, key="GenderYr",value="Death_Rate") %>%
+  separate(GenderYr, into = c("Gender", "Year"), sep=", ") %>%
+  mutate(Age = "All Ages") %>%
+  select(State,Location,code,Age,Gender,Year,Death_Rate) 
+
+
+ocdr_tidy <- ocdr_orig %>%
+  mutate(code = codes$code[match(ocdr_orig$State,codes$state)]) %>%
+  filter(!is.na(State)) %>%
+  gather(`All Ages, 2012`, `All Ages, 2014`, `Age 0-20, 2012`,`Age 0-20, 2014`,`Age 21-34, 2012`,`Age 21-34, 2014`,
+         `Age 35-54, 2012`,`Age 35-54, 2014`,`Age 55+, 2012`,`Age 55+, 2014`, key="AgeYr",value="Death_Rate") %>%
+  separate(AgeYr, into = c("Age", "Year"), sep=", ") %>%
+  mutate(Gender = "All Genders") %>%
+  select(State,Location,code,Age,Year,Gender,Death_Rate) %>%
+  rbind(ocdr_gender)
+
+save(ocdr, ocdr_gender, ocdr_tidy,file="data/deathrate.Rdata")
+
+
+
+
+
